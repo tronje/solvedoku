28eb8e80fd7c622aab07934c6414868f7d6f7724
tronje <tronjekrabbe@gmail.com>
2013-10-17 13:49:04 +0200

    changed solvedoku, it is incomplete and does not work

--- solvedoku.py
+++ solvedoku.py
@@ -1,30 +1,45 @@
-l = []
+rows = []
+squares = []
+cols = []
 
-def load():
+def loadrows():
   with open('testsudoku.txt', 'r') as f:
     read = f.readlines()
     for s in read:
-      l.append(list(s))
-    for e in l:
+      rows.append(list(s))
+    for e in rows:
       if '\n' in e:
         e.pop()
-  return l
+  return rows
+
+def loadsquares():
+  with open('testsudoku', 'r') as f:
+    read = f.read()
+    for s in read:
+      if s == '\n':
+        
+
+def initialize():
+  for e1 in rows:
+    for i in range(1,3):
+      cols.append([e1])
+
 
 def prepare():
   helper = [1,2,3,4,5,6,7,8,9]
-  for e1 in l:
+  for e1 in rows:
     for e2 in e1:
       if e2 != 0:
         rem(helper, e2)
         e2 = [e2]
-  for e1 in l:
-    for e2 in e1:
-      if not isinstance(e2, list):
-        e2 = helper
+  # for e1 in l:
+  #   for e2 in e1:
+  #     if not isinstance(e2, list):
+  #       e2 = helper
 
 def solve():
   
 
 
 def rem(l, val):
-   return [value for value in l if value != val]
+  return [value for value in l if value != val]
